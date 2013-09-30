498a18039881ea694ccf459804605bf870167e36
Daniel X Moore <yahivin@gmail.com>
2013-09-29 17:51:13 -0700

    :top::beetle: Updated in browser at strd6.github.io/editor

---
+++ master.js
@@ -0,0 +1,36 @@
+(function() {
+  var Runtime;
+
+  Runtime = function(pkg) {
+    var currentNode, promo;
+    currentNode = function() {
+      var target;
+      target = document.documentElement;
+      while (target.childNodes.length && target.lastChild.nodeType === 1) {
+        target = target.lastChild;
+      }
+      return target.parentNode;
+    };
+    promo = function() {
+      return console.log("%c You should meet my creator " + pkg.progenitor.url, "background: #000;\ncolor: white;\nfont-size: 2em;\nline-height: 2em;\npadding: 10px 100px;\nmargin-bottom: 1em;\ntext-shadow:\n  0 0 0.05em #fff,\n  0 0 0.1em #fff,\n  0 0 0.15em #fff,\n  0 0 0.2em #ff00de,\n  0 0 0.35em #ff00de,\n  0 0 0.4em #ff00de,\n  0 0 0.5em #ff00de,\n  0 0 0.75em #ff00de;'");
+    };
+    return {
+      boot: function() {
+        var root;
+        root = currentNode();
+        promo();
+        return root;
+      },
+      applyStyleSheet: function(root, name) {
+        var styleNode;
+        styleNode = document.createElement("style");
+        styleNode.innerHTML = require(name);
+        styleNode.className = name;
+        return root.appendChild(styleNode);
+      }
+    };
+  };
+
+  module.exports = Runtime;
+
+}).call(this);
